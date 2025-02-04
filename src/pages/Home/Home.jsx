da661e0f06d9e3c7f809590ead93a19a808f9676
kiwiscode <ayktkav@gmail.com>
2025-02-05 00:21:06 +0100

    crazy hover problem fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,16 @@ import { ThemeContext } from "../../context/ThemeContext";
 
 export default function Home() {
   const { themeName } = useContext(ThemeContext);
+  console.log("theme name:", themeName);
   return (
     <main
-      className={`text-[${
-        themeName === "dark-theme" && "#ffffff"
-      }] px-[36px] mt-[60px]`}
+      style={{
+        color: themeName === "dark-theme" && "#ffffff",
+      }}
+      className="chirp-regular-font px-[36px] mt-[60px]"
     >
       <div
         style={{
-          //   fontFamily: '"Shadows Into Light", cursive',
           fontSize: "2rem",
         }}
       >
