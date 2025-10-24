347a6846686ed60dd574f52625f0529ca76d59d0
kiwiscode <ayktkav@gmail.com>
2025-10-24 03:05:01 +0200

    refactor: OnOffSwitchButton layout and add wrapper for smooth flex alignment

--- src/component/OnOffSwitchButton/OnOffSwitchButton.jsx
+++ src/component/OnOffSwitchButton/OnOffSwitchButton.jsx
@@ -3,23 +3,27 @@ import lightsOffAsset from "../../assets/sounds/dark-mode-active.mp3";
 import lightsOnAsset from "../../assets/sounds/light-mode-active.mp3";
 import "./OnOffSwitchButton.css";
 import { ThemeContext } from "../../context/ThemeContext";
+import { GlobalContext } from "../../context/AppContext";
 
 export default function OnOffSwitchButton() {
   const { toggleTheme, themeName } = useContext(ThemeContext);
+  const { barPosition } = useContext(GlobalContext);
   const lightsOff = new Audio(lightsOffAsset);
   const lightsOn = new Audio(lightsOnAsset);
   return (
-    <div className={`switch-controller-- ${themeName}`}>
-      <div className={`switchyyy ${themeName}`}>
-        <div
-          onClick={() => {
-            if (themeName === "light-theme") lightsOff.play();
-            if (themeName === "dark-theme") lightsOn.play();
+    <div className={`switch-controller--  ${themeName}`}>
+      <div className={`wrapper-- ${barPosition}`}>
+        <div className={`switchyyy ${themeName} `}>
+          <div
+            onClick={() => {
+              if (themeName === "light-theme") lightsOff.play();
+              if (themeName === "dark-theme") lightsOn.play();
 
-            toggleTheme();
-          }}
-          className={`clicky-meee ${themeName}`}
-        ></div>
+              toggleTheme();
+            }}
+            className={`clicky-meee ${themeName}`}
+          ></div>
+        </div>
       </div>
     </div>
   );
