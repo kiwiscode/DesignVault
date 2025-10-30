5d5a59789e1624fbe65260036cb5b298f4bd40e7
kiwiscode <ayktkav@gmail.com>
2025-10-30 19:52:28 +0100

    fix: adjust BottomNavigationBar icon stroke width

--- src/component/BottomNavigationBar/BottomNavigationBar.jsx
+++ src/component/BottomNavigationBar/BottomNavigationBar.jsx
@@ -129,7 +129,7 @@ export default function BottomNavigationBar() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            stroke-width="2"
+            stroke-width="1.5"
             d="m7 8l-4 4l4 4m10-8l4 4l-4 4M14 4l-4 16"
           />
         </svg>
